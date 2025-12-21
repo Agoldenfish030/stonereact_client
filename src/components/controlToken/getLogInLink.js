284f905f05942ba1af9db6fc9d641268603e15c2
Agoldenfish030 <rain20854@gmail.com>
2025-12-22 02:29:38 +0800

    test link

--- src/components/controlToken/getLogInLink.js
+++ src/components/controlToken/getLogInLink.js
@@ -1,6 +1,7 @@
 const logInLink = async()=>{
     try{
         const response = await fetch("https://toomuchstonestodo.onrender.com/logInLink");
+        console.log(response);
         const link = await response.json();
         window.location.href = link.reLink;
     }catch(err){
