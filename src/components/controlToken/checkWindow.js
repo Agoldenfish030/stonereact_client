3cc3136095f1cd1f8924a5a29bea49ac4a6f4564
Agoldenfish030 <rain20854@gmail.com>
2025-12-22 22:44:19 +0800

    debug delete request3

--- src/components/controlToken/checkWindow.js
+++ src/components/controlToken/checkWindow.js
@@ -1,4 +1,8 @@
-window.addEventListener('load', ()=>{});
+window.addEventListener('load', ()=>{
+    const location = useLocation();
+    const pathname = location.pathname;
+    if(!sessionStorage.length && pathname != '/LogInOut') window.location.replace('https://stonereact-client.vercel.app/LogInOut');
+});
 
 window.addEventListener('beforeunload', async()=>{
     const state = sessionStorage.getItem('userState');
