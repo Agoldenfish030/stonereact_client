9e5e7579f437c55d334bb3f84b18645cac5e76d4
Agoldenfish030 <rain20854@gmail.com>
2025-12-22 22:17:15 +0800

    debug delete request2

--- src/components/controlToken/checkWindow.js
+++ src/components/controlToken/checkWindow.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', ()=>{});
 
 window.addEventListener('beforeunload', async()=>{
-    const stateObj = sessionStorage.getItem('userState');
-    const state = stateObj.json();
+    const state = sessionStorage.getItem('userState');
     const response = await fetch('https://toomuchstonestodo.onrender.com/userToken', {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
