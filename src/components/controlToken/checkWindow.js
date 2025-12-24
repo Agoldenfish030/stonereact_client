77f6c44e7ec11fa6066728cac8f16d20f961bd5c
Agoldenfish030 <rain20854@gmail.com>
2025-12-24 17:48:33 +0800

    debug delete request plz let me pass

--- src/components/controlToken/checkWindow.js
+++ src/components/controlToken/checkWindow.js
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 const useCheckWindow = ()=>{
     useEffect(()=>{
         const token = sessionStorage.getItem('userState');
-        if(!token && window.location.href != 'https://stonereact-client.vercel.app/LogInOut'){
+        if(!token
+            && window.location.href != 'https://stonereact-client.vercel.app/LogInOut'
+            && !window.location.hash.includes('token=')){
             window.alert("您尚未登入！");
             window.location.href = 'https://stonereact-client.vercel.app/LogInOut';
         }
