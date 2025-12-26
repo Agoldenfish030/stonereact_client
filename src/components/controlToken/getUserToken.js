7a33fe8ab59181edc9abd922b4666849cbfc2dd1
Agoldenfish030 <rain20854@gmail.com>
2025-12-26 17:28:01 +0800

    debug a bug that maybe someday will appear

--- src/components/controlToken/getUserToken.js
+++ src/components/controlToken/getUserToken.js
@@ -1,26 +1,29 @@
 import { useEffect } from "react";
 
 const useUserToken = ()=>{
-    useEffect(async() => {
-    // 從網址 hash 中抓取 token (#token=xxxx)
-        const hash = window.location.hash;
-        const token = new URLSearchParams(hash.replace('#', '?')).get('token');
+    useEffect(() => {
+        const getUserToken = async()=>{
+            // 從網址 hash 中抓取 token (#token=xxxx)
+            const hash = window.location.hash;
+            const token = new URLSearchParams(hash.replace('#', '?')).get('token');
 
-        if (token){
-            // 傳送到 Express 後端
-            const response = await fetch('https://toomuchstonestodo.onrender.com/userToken', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ token })
-            });
-            const stateObj = await response.json();
-            const state = stateObj.userState;
-            sessionStorage.setItem('userState', state);
+            if (token){
+                // 傳送到 Express 後端
+                const response = await fetch('https://toomuchstonestodo.onrender.com/userToken', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ token })
+                });
+                const stateObj = await response.json();
+                const state = stateObj.userState;
+                sessionStorage.setItem('userState', state);
 
-            //登入成功後跳轉回主畫面
-            window.location.replace('https://stonereact-client.vercel.app/');
+                //登入成功後跳轉回主畫面
+                window.location.replace('https://stonereact-client.vercel.app/');
+            }
         }
-    });
+        getUserToken();
+    }, []);
 }
 
 export default useUserToken;
