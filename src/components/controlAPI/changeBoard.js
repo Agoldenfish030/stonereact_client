9fd8f7ddb23c953d8748131b066cb9fbd4a0b9a1
Agoldenfish030 <rain20854@gmail.com>
2025-12-26 10:57:00 +0800

    changeBoard: add return cardsList

--- src/components/controlAPI/changeBoard.js
+++ src/components/controlAPI/changeBoard.js
@@ -1,9 +1,11 @@
 const useChangeBoard = async(userState, mainBoardID)=>{
-    await fetch('https://toomuchstonestodo.onrender.com/userToken/changeMainBoard', {
+    const response = await fetch('https://toomuchstonestodo.onrender.com/userToken/changeMainBoard', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: {userState, mainBoardID}
     });
+    const newCardsList = response.newCardsList;
+    return newCardsList;
 }
 
 export default useChangeBoard;
