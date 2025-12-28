1852889990873d8f67bdef18d536f76643ad61c4
Agoldenfish030 <rain20854@gmail.com>
2025-12-28 18:41:13 +0800

    別廢話讓我看看

--- src/components/MainArea/Home.js
+++ src/components/MainArea/Home.js
@@ -53,6 +53,7 @@ const Home = ({ xp, level, onTaskComplete, userState }) => {
             setCurrentBoard(selected);
             setShowInitSelect(false); 
             try {
+                console.log(newBoardID);
                 const newCardsList = await fetchChangeBoard(userState, newBoardID);
                 setTasks(newCardsList || []); 
             } catch (err) {
