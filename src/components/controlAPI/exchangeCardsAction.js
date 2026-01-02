50382c141c98dff90dad51a02bba846d7844fd07
Agoldenfish030 <rain20854@gmail.com>
2026-01-02 17:15:48 +0800

    try to get cards action

--- src/components/controlAPI/exchangeCardsAction.js
+++ src/components/controlAPI/exchangeCardsAction.js
@@ -1,13 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 const socket = io('https://toomuchstonestodo.onrender.com');
 const useExchangeCardsAction = ()=>{
+    const [resCard, setResCard] = useState(null);
+
     useEffect(() => {
-        socket.on('cardChange');
+        socket.on('cardChange', (data)=>{
+            setResCard(data);
+        });
 
         return () => socket.off('cardChange');
     }, []);
+
+    return resCard;
 }
 
 export default useExchangeCardsAction;
